fe566af4d26780dabbb9cce945122b8fb2c204f8
Markus Sabadello <markus@danubetech.com>
2018-05-19 19:52:57 +0200

    Add txrefextEncode, txrefextDecode, and supporting tests

--- src/main/java/info/weboftrust/txrefconversion/TxrefConverter.java
+++ src/main/java/info/weboftrust/txrefconversion/TxrefConverter.java
@@ -39,6 +39,10 @@ public class TxrefConverter {
 		return instance;
 	}
 
+	/*
+	 * txref encode / decode
+	 */
+
 	public String txrefEncode(Chain chain, long blockHeight, long blockIndex) {
 
 		byte magic = chain == Chain.MAINNET ? MAGIC_BTC_MAINNET : MAGIC_BTC_TESTNET;
@@ -139,6 +143,128 @@ public class TxrefConverter {
 
 		return new ChainAndBlockLocation(chain, blockHeight, blockIndex);
 	}
+
+	/*
+	 * txrefext encode / decode
+	 */
+
+	public String txrefextEncode(Chain chain, long blockHeight, long blockIndex, long utxoIndex) {
+
+		byte magic = chain == Chain.MAINNET ? MAGIC_BTC_MAINNET : MAGIC_BTC_TESTNET;
+		byte[] prefix = chain == Chain.MAINNET ? TXREF_BECH32_HRP_MAINNET_BYTES : TXREF_BECH32_HRP_TESTNET_BYTES;
+		boolean nonStandard = chain != Chain.MAINNET;
+
+		byte[] shortId;
+		shortId = nonStandard ? new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 } : new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+		if ((nonStandard && (blockHeight > 0x1FFFFF || blockIndex > 0x1FFF || utxoIndex > 0x1FFF || magic > 0x1F)) ||
+				(nonStandard && (blockHeight > 0x3FFFFFF || blockIndex > 0x3FFFF || utxoIndex > 0x1FFF || magic > 0x1F))) {
+
+			return null;
+		}
+
+		/* set the magic */
+		shortId[0] = magic;
+
+		/* make sure the version bit is 0 */
+		shortId[1] &= ~(1 << 0);
+
+		shortId[1] |= ((blockHeight & 0xF) << 1);
+		shortId[2] |= ((blockHeight & 0x1F0) >> 4);
+		shortId[3] |= ((blockHeight & 0x3E00) >> 9);
+		shortId[4] |= ((blockHeight & 0x7C000) >> 14);
+
+		if (nonStandard) {
+			// use extended blockheight (up to 0x3FFFFFF)
+			// use extended txpos (up to 0x3FFFF)
+			shortId[5] |= ((blockHeight & 0xF80000) >> 19);
+			shortId[6] |= ((blockHeight & 0x3000000) >> 24);
+
+			shortId[6] |= ((blockIndex & 0x7) << 2);
+			shortId[7] |= ((blockIndex & 0xF8) >> 3);
+			shortId[8] |= ((blockIndex & 0x1F00) >> 8);
+			shortId[9] |= ((blockIndex & 0x3E000) >> 13);
+			shortId[10] |= ((utxoIndex & 0x1F));
+			shortId[11] |= ((utxoIndex & 0x3E0) >> 5);
+			shortId[12] |= ((utxoIndex & 0x1C00) >> 10);
+		} else {
+			shortId[5] |= ((blockHeight & 0x180000) >> 19);
+			shortId[5] |= ((blockIndex & 0x7) << 2);
+			shortId[6] |= ((blockIndex & 0xF8) >> 3);
+			shortId[7] |= ((blockIndex & 0x1F00) >> 8);
+			shortId[8] |= ((utxoIndex & 0x1F));
+			shortId[9] |= ((utxoIndex & 0x3E0) >> 5);
+			shortId[10] |= ((utxoIndex & 0x1C00) >> 10);
+		}
+
+		String result = Bech32.bech32Encode(prefix, shortId);
+
+		int breakIndex = prefix.length + 1;
+		String finalResult = result.substring(0, breakIndex) + "-" +
+				result.substring(breakIndex, breakIndex + 4) + "-" +
+				result.substring(breakIndex + 4, breakIndex + 8) + "-" +
+				result.substring(breakIndex + 8, breakIndex + 12) + "-" +
+				result.substring(breakIndex + 12, result.length());
+
+		return finalResult;
+	}
+
+	public String txrefextEncode(ChainAndBlockLocationAndUtxoIndex chainAndBlockLocationAndUtxoIndex) {
+
+		return this.txrefEncode(chainAndBlockLocationAndUtxoIndex.getChain(), chainAndBlockLocationAndUtxoIndex.getBlockHeight(), chainAndBlockLocationAndUtxoIndex.getBlockIndex());
+	}
+
+	public ChainAndBlockLocationAndUtxoIndex txrefextDecode(String bech32Tx) {
+
+		String stripped = bech32Tx.replace("-", "");
+
+		HrpAndData result = Bech32.bech32Decode(stripped);
+		if (result == null) {
+			return null;
+		}
+		byte[] buf = result.getData();
+
+		byte chainMarker = buf[0];
+		boolean nonStandard = chainMarker != MAGIC_BTC_MAINNET;
+
+		long bStart = (buf[1] >> 1) |
+				(buf[2] << 4) |
+				(buf[3] << 9) |
+				(buf[4] << 14);
+
+		long blockHeight = 0;
+		long blockIndex = 0;
+		long utxoIndex = 0;
+
+		if (nonStandard) {
+			blockHeight = bStart | (buf[5] << 19);
+			blockHeight |= ((buf[6] & 0x03) << 24);
+
+			blockIndex = (buf[6] & 0x1C) >> 2;
+			blockIndex |= (buf[7] << 3);
+			blockIndex |= (buf[8] << 8);
+			blockIndex |= (buf[9] << 13);
+			utxoIndex = buf[10];
+			utxoIndex |= (buf[11] << 5);
+			utxoIndex |= (buf[12] << 10);
+		} else {
+			blockHeight = bStart | ((buf[5] & 0x03) << 19);
+			blockIndex = (buf[5] & 0x1C) >> 2;
+			blockIndex |= (buf[6] << 3);
+			blockIndex |= (buf[7] << 8);
+			utxoIndex = buf[8];
+			utxoIndex |= (buf[9] << 5);
+			utxoIndex |= (buf[10] << 10);
+		}
+
+		Chain chain = chainMarker == MAGIC_BTC_MAINNET ? Chain.MAINNET : Chain.TESTNET;
+
+		return new ChainAndBlockLocationAndUtxoIndex(chain, blockHeight, blockIndex, utxoIndex);
+	}
+
+	/*
+	 * txid <-> txref
+	 */
 
 	public String txidToTxref(String txid, Chain chain) throws IOException {
 
--- src/test/java/info/weboftrust/txrefconversion/TxrefConverterTest.java
+++ src/test/java/info/weboftrust/txrefconversion/TxrefConverterTest.java
@@ -22,6 +22,10 @@ public class TxrefConverterTest extends TestCase {
 
 	}
 
+	/*
+	 * txref encode / decode
+	 */
+
 	private static Object[][] tests1 = new Object[][] {
 		new Object[] { Chain.MAINNET, "tx1-rqqq-qqqq-qmhu-qk", (long) 0, (long) 0 },	
 		new Object[] { Chain.MAINNET, "tx1-rzqq-qqqq-uvlj-ez", (long) 1, (long) 0 },	
@@ -38,7 +42,7 @@ public class TxrefConverterTest extends TestCase {
 		new Object[] { Chain.TESTNET, "txtest1-xyv2-xzyq-qqm5-tyke", (long) 1152194, (long) 1 }	
 	};
 
-	public void testTxRefEncode() throws Exception {
+	public void testTxrefEncode() throws Exception {
 
 		for (Object[] test : tests1) {
 
@@ -47,7 +51,7 @@ public class TxrefConverterTest extends TestCase {
 		}
 	}
 
-	public void testTxRefDecode() throws Exception {
+	public void testTxrefDecode() throws Exception {
 
 		for (Object[] test : tests1) {
 
@@ -58,14 +62,56 @@ public class TxrefConverterTest extends TestCase {
 		}
 	}
 
+	/*
+	 * txrefext encode / decode
+	 */
+
 	private static Object[][] tests2 = new Object[][] {
+		new Object[] { Chain.MAINNET, "tx1-rqqq-qqqq-qqqu-au7hl", (long) 0, (long) 0, (long) 0 },
+		new Object[] { Chain.MAINNET, "tx1-rqqq-qqqq-yrq9-mqh4w", (long) 0, (long) 0, (long) 100 },
+		new Object[] { Chain.MAINNET, "tx1-rqqq-qqqq-ll8t-emcac", (long) 0, (long) 0, (long) 0x1FFF },
+		new Object[] { Chain.MAINNET, "tx1-rqqq-qull-qqq5-ktx95", (long) 0, (long) 0x1FFF, (long) 0 },
+		new Object[] { Chain.MAINNET, "tx1-rqqq-qull-yrqd-sh089", (long) 0, (long) 0x1FFF, (long) 100 },
+		new Object[] { Chain.MAINNET, "tx1-r7ll-lrqq-qqqm-m5vjv", (long) 0x1FFFFF, (long) 0, (long) 0 },
+		new Object[] { Chain.MAINNET, "tx1-r7ll-llll-qqqn-sr5q8", (long) 0x1FFFFF, (long) 0x1FFF, (long) 0 },
+		new Object[] { Chain.MAINNET, "tx1-r7ll-llll-yrq2-klazk", (long) 0x1FFFFF, (long) 0x1FFF, (long) 100 },
+		new Object[] { Chain.MAINNET, "tx1-rjk0-u5ng-qqq8-lsnk3", (long) 0x71F69, (long) 0x89D, (long) 0 },
+		new Object[] { Chain.MAINNET, "tx1-rjk0-u5ng-dqqa-9wk8d", (long) 0x71F69, (long) 0x89D, (long) 13 }
+	};
+
+	public void testTxrefextEncode() throws Exception {
+
+		for (Object[] test : tests2) {
+
+			String result = txrefConverter.txrefextEncode((Chain) test[0], (long) test[2], (long) test[3], (long) test[4]);
+			assertEquals((String) test[1], result);
+		}
+	}
+
+	public void testTxrefextDecode() throws Exception {
+
+		for (Object[] test : tests2) {
+
+			ChainAndBlockLocationAndUtxoIndex result = txrefConverter.txrefextDecode((String) test[1]);
+			assertEquals((Chain) test[0], result.getChain());
+			assertEquals((long) test[2], result.getBlockHeight());
+			assertEquals((long) test[3], result.getBlockIndex());
+			assertEquals((long) test[4], result.getUtxoIndex());
+		}
+	}
+
+	/*
+	 * txid <-> txref
+	 */
+
+	private static Object[][] tests3 = new Object[][] {
 		new Object[] { Chain.TESTNET, "f8cdaff3ebd9e862ed5885f8975489090595abe1470397f79780ead1c7528107", "txtest1-xyv2-xzyq-qqm5-tyke" },
 		new Object[] { Chain.MAINNET, "016b71d9ec62709656504f1282bb81f7acf998df025e54bd68ea33129d8a425b", "tx1-rk63-uvxf-9pqc-sy" }
 	};
 
 	public void testTxidToTxref() throws Exception {
 
-		for (Object[] test : tests2) {
+		for (Object[] test : tests3) {
 
 			String result = txrefConverter.txidToTxref((String) test[1], (Chain) test[0]);
 			assertEquals((String) test[2], result);
@@ -74,7 +120,7 @@ public class TxrefConverterTest extends TestCase {
 
 	public void testTxrefToTxid() throws Exception {
 
-		for (Object[] test : tests2) {
+		for (Object[] test : tests3) {
 
 			ChainAndTxid result = txrefConverter.txrefToTxid((String) test[2]);
 			assertEquals((Chain) test[0], result.getChain());
